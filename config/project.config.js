826484a1cfa854ff18a514bb94d4f3a047c38ff5
tom1193 <thomas8289@gmail.com>
2017-04-24 02:36:27 -0400

    basename test

--- config/project.config.js
+++ config/project.config.js
@@ -83,7 +83,7 @@ config.globals = {
   '__PROD__'     : config.env === 'production',
   '__TEST__'     : config.env === 'test',
   '__COVERAGE__' : !argv.watch && config.env === 'test',
-  '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
+  'kelaHealth-client-demo/' : JSON.stringify(process.env.BASENAME || '')
 }
 
 // ------------------------------------
